01f2ca040171d9dcc4c7852131f2e6394742cf23
V-Srivatsan <vatsan.programming@gmail.com>
2023-09-20 18:20:16 +0530

    Added comments

--- server/auth.py
+++ server/auth.py
@@ -2,24 +2,30 @@ from .models import User
 from django.contrib.auth.hashers import make_password, check_password
 
 def login(data):
+    # Get all users with entered email
     user = User.objects.filter(email=data['email'])
 
+    # Check if users found
     if len(user) == 0: 
         return { 'valid': False, 'message': 'Email not found! Please make sure you have entered the correct email id!' }
 
+    # Check if user account is activated
     if not user[0].activated:
         return { 'valid': False, 'message': 'Your account has been temporarily disabled! This may be because your account has not yet been verified or due to misuse of the platform...' }
 
+    # Check if password is valid
     if not check_password(data['password'], user[0].password):
         return { 'valid': False, 'message': 'Incorrect password! Please make sure the password you entered is correct!' }
     
     return { 'valid': True }
 
 def signup(data):
+    # Check if email is already linked to given email
     user = User.objects.filter(email=data['email'])
     if len(user) > 0:
         return { 'valid': False, 'message': 'Another account is already using this email! Please provide another!!' }
     
+    # Create user email
     User(
         name=data['name'], nickname=data['nickname'], email=data['email'], 
         password=make_password(data['password'])
--- server/blog.py
+++ server/blog.py
@@ -2,19 +2,23 @@ from .models import *
 import uuid
 
 def create_blog(data, email):
+    # Create and post blog
     Post(
         user=User.objects.get(email=email),
         title=data['title'], content=data['content']
     ).save()
 
 def edit_blog(data, uid):
+    # Find blog and update title and contents
     Post.objects.filter(uid=uuid.UUID(uid)).update(title=data['title'], content=data['content'])
 
 def remove_blog(uid):
+    # Find and delete blog
     Post.objects.filter(uid=uuid.UUID(uid)).delete()
 
 
 def post_reply(data, uid, email):
+    # Create reply after getting user and blog
     Reply(
         user=User.objects.get(email=email),
         post=Post.objects.get(uid=uuid.UUID(uid)),
