262659db9a62e3641f0006c221c3d2748beef76d
Srivatsan-KVP <145278456+Srivatsan-KVP@users.noreply.github.com>
2023-11-10 19:35:11 +0530

    MYSQL connectivity

--- cs_blog/settings.py
+++ cs_blog/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'cs_blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "user": 'root',
+            'password': '1521kvp',
+            'database': 'blog'
+        }
     }
 }
 
